3d6c4af380fe9b721daea97496fcab7b92f2195e
Boble <67345802+Boble1337@users.noreply.github.com>
2024-01-21 01:55:24 +0300

    Update homework24.py

--- homework24.py
+++ homework24.py
@@ -1,1 +1,25 @@
-# Homework24
+# задача
+# реализовать класс
+# Класс Snowflake
+# Атрибуты: размер, форма.
+# Методы: change_size(new_size), change_shape(new_shape).
+# НЕОБЯЗАТЕЛЬНО: Интерфейс для изменения размера и формы снежинки
+
+class Snowflake:
+    def __init__(self, size, shape):
+        self.size = size
+        self.shape = shape
+
+    def change_size(self, new_size):
+        self.size = new_size
+        print(f'размер изменен {self.size}')
+
+    def change_shape(self, new_shape):
+        self.shape = new_shape
+        print(f'форма изменена {self.shape}')
+
+    def display(self):
+        print(f'снежинка: размер - {self.size}, форма - {self.shape}')
+
+snowflake1 = Snowflake(size='маленькая', shape='звезда')
+snowflake1.display()
